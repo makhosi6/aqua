9985022ff269a5efa0e23f158ba4000b5bbc6107
makhosi6 <56253310+makhosi6@users.noreply.github.com>
2020-01-13 04:26:03 +0200

    Add files via upload

---
+++ two.js
@@ -0,0 +1,63 @@
+let bttn = document.querySelector('.bttn');
+let one = document.querySelector('body');
+let two = document.querySelector(".two");
+
+
+bttn.addEventListener('click', myFunction)
+
+function myFunction() {
+
+
+    //two.classList.toggle("twoo");
+
+    console.log(two);
+}
+
+
+// bttn.addEventListener('click', myFunction)
+
+// function myFunction() {
+
+//     document.querySelector(".two").style.left = "35%"
+//     document.querySelector(".two").style.top = "17%"
+//     document.querySelector(".two").style.left = "35%"
+
+//     console.log('work!!!')
+
+
+// };
+
+// bttn.onclick = function() {
+
+//     two.style.left = "35%"
+//     two.style.top = "17%"
+//     two.style.left = "35%"
+
+//     console.log('works!!!')
+// };
+
+// one.addEventListener('click', myFunction)
+
+// function myFunction() {
+
+//     document.querySelector(".two").style.display = "none"
+
+
+//     console.log(one);
+
+
+// };
+
+
+
+// bttn.onmouseenter = function() {
+//     document.querySelector(".two").style.left = "35%"
+//     document.querySelector(".two").style.top = "17%"
+//     document.querySelector(".two").style.left = "35%"
+// };
+
+// let leave = document.querySelector(".two")
+// leave.onmouseleave = function() {
+//     document.querySelector(".two").style.display = "none"
+
+// };
